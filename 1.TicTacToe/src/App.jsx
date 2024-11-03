751ef6a81011a3ae8a67fff6d6f84314deccda32
Baskoro Ramadhan <baskoror725@gmail.com>
2024-11-03 18:38:25 +0700

    import GameBoard comp

--- 1.TicTacToe/src/App.jsx
+++ 1.TicTacToe/src/App.jsx
@@ -1,7 +1,5 @@
-
-
 import Player from "./components/Player"
-
+import GameBoard from "./components/GameBoard"
 function App() {
 
   return (
@@ -11,7 +9,7 @@ function App() {
           <Player initialName='Player1' symbol="X"/>
           <Player initialName='Player2' symbol="O"/>
         </ol>
-        Game board
+        <GameBoard/>
       </div>
     </main>
   )
