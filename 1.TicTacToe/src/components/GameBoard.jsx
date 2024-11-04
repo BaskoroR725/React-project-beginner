fb6d7dcd59405a5c3e93baf42c689ae436d29399
Baskoro Ramadhan <baskoror725@gmail.com>
2024-11-04 21:02:32 +0700

    add game turns log component and refactor some state

--- 1.TicTacToe/src/components/GameBoard.jsx
+++ 1.TicTacToe/src/components/GameBoard.jsx
@@ -1,38 +1,49 @@
-import { useState } from "react";
-
-const InitialGameBoard=[
-    [null,null,null],
-    [null,null,null],
-    [null,null,null],
-];
-
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }){
-    const [ gameBoard , setGameBoard ] = useState(InitialGameBoard);
-
-    function handleSelectSquare(rowIndex,colIndex){
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-        onSelectSquare();
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  
+  export default function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
+  
+    for (const turn of turns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+  
+      gameBoard[row][col] = player;
     }
-
-    return(
-        <ol id="game-board">
-            {gameBoard.map((row,rowIndex) =>(
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((playerSymbol,colIndex) =>(
-                            <li key={colIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex,colIndex)}>
-                                    {playerSymbol}
-                                </button>
-                            </li>
-                        ))}
-                    </ol>
+  
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //   setGameBoard((prevGameBoard) => {
+    //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //     return updatedBoard;
+    //   });
+  
+    //   onSelectSquare();
+    // }
+  
+    return (
+      <ol id="game-board">
+        {gameBoard.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                  <button
+                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                    disabled={playerSymbol !== null}
+                  >
+                    {playerSymbol}
+                  </button>
                 </li>
-            ))}
-        </ol>
-    )
-}
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    );
+  }
