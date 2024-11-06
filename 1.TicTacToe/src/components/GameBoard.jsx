657bd9a3cdc94fbcd55e7b799293bd48cdd89739
Baskoro Ramadhan <baskoror725@gmail.com>
2024-11-06 20:11:55 +0700

    add game over feature and logic winner

--- 1.TicTacToe/src/components/GameBoard.jsx
+++ 1.TicTacToe/src/components/GameBoard.jsx
@@ -1,19 +1,7 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
+
   
-  export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard;
-  
-    for (const turn of turns) {
-      const { square, player } = turn;
-      const { row, col } = square;
-  
-      gameBoard[row][col] = player;
-    }
-  
+  export default function GameBoard({ onSelectSquare, board }) {
+ 
     // const [gameBoard, setGameBoard] = useState(initialGameBoard);
   
     // function handleSelectSquare(rowIndex, colIndex) {
@@ -28,7 +16,7 @@ const initialGameBoard = [
   
     return (
       <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
+        {board.map((row, rowIndex) => (
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) => (
